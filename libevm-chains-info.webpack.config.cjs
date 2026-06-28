ea2a8cf223a42239e03201c963226a918d718a99
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-06-28 09:54:02 +0000

    mm

--- libevm-chains-info.webpack.config.cjs
+++ libevm-chains-info.webpack.config.cjs
@@ -1,25 +1,28 @@
-/**    ----------------------------------------------------------------------
- *     libbin2txt.webpack.config.cjs
- *     ----------------------------------------------------------------------
+/**    ---------------------------------------------------------
+ *     libevm-chains-info.webpack.config.cjs
+ *     ---------------------------------------------------------
  *     Copyright ©
  *       Pellegrino Prevete
- *         2025, 2026
+ *         2024, 2025, 2026
  * 
  *     All rights reserved
- *     ----------------------------------------------------------------------
+ *     ---------------------------------------------------------
  * 
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ *     This program is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either
+ *     version 3 of the License, or (at your option) any later
+ *     version.
  * 
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     This program is distributed in the hope that it will be
+ *     useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE.
+ *     See the GNU General Public License for more details.
  * 
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     You should have received a copy of the
+ *     GNU General Public License along with this program.
+ *     If not, see <https://www.gnu.org/licenses/>.
  */
 
 const
@@ -47,21 +50,19 @@ const
       _output_file_name
 };
 const
-  _utils_ignore =
-  { resourceRegExp:
-      /^utils$/ };
-const
-  _web_worker_ignore =
-  { resourceRegExp:
-      /^web-worker$/ };
+  _node_fs_ignore = {
+    resourceRegExp:
+      /^node:fs$/
+};
 const
   _yargs_ignore =
   { resourceRegExp:
       /^yargs$/ };
 const
-  _yargs_helpers_ignore =
-  { resourceRegExp:
-      /^yargs\/helpers$/ };
+  _yargs_helpers_ignore = {
+    resourceRegExp:
+      /^yargs\/helpers$/
+  };
 const
   _webpack =
     require(
@@ -70,13 +71,9 @@ const
   _ignore_plugin =
     _webpack.IgnorePlugin; 
 const
-  _utils_ignore_plugin =
+  _node_fs_ignore_plugin =
     new _ignore_plugin(
-          _utils_ignore);
-const
-  _web_worker_ignore_plugin =
-    new _ignore_plugin(
-          _web_worker_ignore);
+          _node_fs_ignore);
 const
   _yargs_ignore_plugin =
     new _ignore_plugin(
@@ -95,18 +92,22 @@ module.exports = {
   },
   resolve: {
     alias: {
+      "node:fs":
+        _path.resolve(
+          __dirname,
+          'node_modules/opfs/opfs'),
+      "fs":
+        _path.resolve(
+          __dirname,
+          "node_modules/fs/fs"),
       "evm-contracts-tools":
         _path.resolve(
           __dirname,
-          'node_modules/evm-contract-tools/evm-contracts-tools'),
+          "node_modules/evm-contracts-tools/evm-contracts-tools"),
       "evm-wallet.js":
         _path.resolve(
           __dirname,
           'node_modules/evm-wallet.js/libevm-wallet'),
-      "fs":
-        _path.resolve(
-          __dirname,
-          'node_modules/fs/fs'),
       "path":
         _path.resolve(
           __dirname,
@@ -143,6 +144,7 @@ module.exports = {
     { yargs:
         'yargs' },
   plugins: [
+    _node_fs_ignore_plugin,
     _yargs_ignore_plugin,
     _yargs_helpers_ignore_plugin
   ]
